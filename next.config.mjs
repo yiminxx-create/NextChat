c5440ed729bcfe7574bf2164ae2c22705a0aa01b
yiminxx-create <yiminxx@gmail.com>
2026-03-11 15:20:40 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,7 @@ console.log("[Next] build with chunk: ", !disableChunk);
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack(config) {
+    config.cache = false;
     config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"],
