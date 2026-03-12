cefc350bb67fe5dd5d19c0076bc33e1258dfd0ad
yiminxx-create <yiminxx@gmail.com>
2026-03-12 20:27:23 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
-import { Home } from "./components/home";
+import Home from "./components/home";  // ← 正确方式，默认导入
 import { getServerSideConfig } from "./config/server";
 
 const serverConfig = getServerSideConfig();
